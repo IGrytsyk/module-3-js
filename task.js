f7bf860c06a7d385414439b4aa955a93fbc726cc
IGrytsyk <igrutsuk@gmail.com>
2023-08-26 18:39:01 +0300

    1

---
+++ task.js
@@ -0,0 +1,36 @@
+const arr = [1, 2, 3, 4, 4];
+console.log(arr);
+
+
+
+// Виконай рефакторинг рішення попереднього завдання, додавши в цикл for...in перевірку на власну властивість.
+
+// Оголошена змінна advert.
+// Значення змінної advert - це об'єкт.
+// Оголошена змінна apartment.
+// Значення змінної apartment - це об'єкт.
+// Оголошена змінна keys.
+// Значення змінної keys - це масив ["descr", "rating", "price"].
+// Оголошена змінна values.
+// Значення змінної values - це масив ["Spacious apartment in the city center", 4, 2153].
+
+// const keys = ["descr", "rating", "price"];
+// const values = ["Spacious apartment in the city center", 4, 2153];
+// const advert = {
+//   service: "apt",
+// };
+// console.log(values);
+
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+
+//   keys.push(key);
+//   values.push(apartment[key]);
+// console.log(key);
+//   // Change code above this line
+// }
